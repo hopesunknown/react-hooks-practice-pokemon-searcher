eaeb9eb72add50a4b8de0ea59e217eaa6892a48a
Jennifer Tchai <jentchai@gmail.com>
2022-01-21 09:31:15 -0500

    commit tree dfa75918f678a6fb2551c30cba790418b6f32618

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({pokeData}) {
+ const [isClicked, setIsClicked] = useState(true)
+
+ function handleClick() {
+   setIsClicked(!isClicked)
+ }
+
   return (
     <Card>
       <div>
-        <div className="image">
-          <img alt="oh no!" />
+        <div className="image" onClick={handleClick} >
+          <img src={ isClicked ? pokeData.sprites.front : pokeData.sprites.back } alt={pokeData.name}/>
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{pokeData.name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {pokeData.hp} hp
           </span>
         </div>
       </div>
